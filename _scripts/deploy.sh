ad1cf24f6a5f293073b24a26355c1869792addc8
nigelpain <nigel.pain@digital.landregistry.gov.uk>
2016-12-29 11:54:46 +0000

    trying something different for deploy

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 set -x
 if [ $TRAVIS_BRANCH == 'master' ] ; then
+    mkdir deploy
+    cp -R /html /deploy
+    cd deploy
+    git init
+
     git remote add do_server "mr_monkey@178.62.24.126:/var/www"
     git config user.name "nigelpain"
     git config user.email "nigel.pain@digital.landregistry.gov.uk"
 
+    git add .
+    git commit -m "Deployment"
     git push --force do_server master
 else
     echo "Not deploying, since this branch isn't master."
