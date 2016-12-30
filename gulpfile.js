5f179aaf5d663bcdc19429725edace1c57e0bd6d
nigelpain <nigel.pain@digital.landregistry.gov.uk>
2016-12-30 11:27:22 +0000

    added sass compiler to gulp and watcher task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-gulp.task('hello', function() {
-  console.log("Hi I'm a gulp task");
+// Watch for any file changes and reload page
+gulp.task('watch', function() {
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+});
+
+// Compile all sass files into css
+gulp.task('sass', function() {
+  return gulp.src('app/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
 });
