133156e296c3db5958def9fe57c62bf6fddf0a38
nigelpain <nigel.pain@digital.landregistry.gov.uk>
2017-01-20 13:12:53 +0000

    amended gulp script to proxy MAMP server for browsersync instead

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,12 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
-var php = require('gulp-connect-php');
 var sass = require('gulp-sass');
 var runSequence = require('run-sequence');
 
 // Run the various gulp tasks in order due to dependencies
 gulp.task('default', function(callback) {
-  runSequence('php', 'browserSync', 'sass', 'watch', callback)
-})
+  runSequence('browserSync', 'sass', 'watch', callback)
+});
 
 // Compile all sass files into css
 gulp.task('sass', function() {
@@ -16,23 +15,18 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('app/css'))
     .pipe(browserSync.reload({
       stream: true
-    }));
-})
+    }))
+});
 
-// Start up a php server on localhost on the specified port
-gulp.task('php', function() {
-  php.server({ base: 'app', port: 8010, keepalive: true })
-})
-
-// Proxy the php server using browsersync
+// Proxy the server run using MAMP default settings
 gulp.task('browserSync', function() {
   browserSync({
-    proxy: '127.0.0.1:8010',
+    proxy: '127.0.0.1:8888',
     port: 8080,
     open: false,
     notify: false
   })
-})
+});
 
 // Watch for any code changes and reload the page
 gulp.task('watch', function() {
@@ -40,4 +34,4 @@ gulp.task('watch', function() {
   gulp.watch('app/scss/**/*.scss', ['sass']);
   gulp.watch('app/*.html', browserSync.reload);
   gulp.watch('app/js/**/*.js', browserSync.reload);
-})
+});
